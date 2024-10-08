9173c24caf37eafee26f740eab8b528d0a7d0fcc
haneullee <skyee46@gmail.com>
2024-10-08 15:58:39 +0900

    수정

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-k+wqi79lk-p45@+&0$xe68c@k9eieg@_j^fxdbtz3p)af)c)%u"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'haneullee.pythonanywhere.com']
 
 
 # Application definition
